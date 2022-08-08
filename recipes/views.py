c9e65cb98cca15c7f10c7e079f48b3f7b0316ec1
Tatiani-Cunha <tatianicpsantana@gmail.com>
2022-08-07 23:19:27 -0300

    configurando urls e views

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+#from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse('HOME')
+
+
+def contato(request):
+    return HttpResponse('CONTATO')
+
+
+def sobre(request):
+    return HttpResponse('SOBRE')
