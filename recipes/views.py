e32fea7d6e295d7f2eb75ac229f85233e26fe4cd
Tatiani-Cunha <tatianicpsantana@gmail.com>
2022-08-08 16:19:09 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,10 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from django.shortcuts import render
-
-#from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Tatiani Cunha',
+    })
