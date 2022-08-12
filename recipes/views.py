66ccebaa1b967b85febb9fed9c27d7c05326f60b
Tatiani-Cunha <tatianicpsantana@gmail.com>
2022-08-11 22:09:11 -0300

    ajustando algumas configurações do views e templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,4 @@
-#from django.http import HttpResponse
 from django.shortcuts import render
-
-# Create your views here.
 
 
 def home(request):
@@ -11,6 +8,6 @@ def home(request):
 
 
 def recipe(request, id):
-return render(request, 'recipes/pages/recipe-view.html', context={
-    'name': 'Tatiani Cunha',
-})
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Tatiani Cunha',
+    })
